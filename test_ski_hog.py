408eb4a1fa3b62ef38373ed1529724e48d94d205
You <you@example.com>
2016-08-26 17:55:45 +0900

    修正

--- test_ski_hog.py
+++ test_ski_hog.py
@@ -4,9 +4,21 @@ Created on Wed Aug 24 15:12:14 2016
 
 @author: d2713
 """
-
+from skimage import io
 from skimage.feature import hog
 import matplotlib.pyplot as plt
 
-plt.imshow()
+image = io.imread("crop.png",0)
+
+orientations = 9
+pixels_per_cell = (8, 8)
+cells_per_block = (3, 3)
+
+import skimage.color
+gimg = skimage.color.rgb2gray(image)
+gimg = skimage.img_as_ubyte(gimg)
+
+h = hog(gimg, orientations=orientations, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block)
+
+plt.imshow(gimg,cmap="gray")
 plt.show()
