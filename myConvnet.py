95381e7395378e608a1226a610c244f69d369a40
Your Name <you@example.com>
2016-09-13 09:21:13 +0900

    途中経過をわかりやすくするためprint文追記

--- myConvnet.py
+++ myConvnet.py
@@ -32,22 +32,23 @@ print(73*4)
 #image = image[:,:,:,np.newaxis]
 image = np.transpose(image,(2,0,1))
 image = image[np.newaxis,:,:,:]
-print(image.shape)
+print("input image:",image.shape)
 
 #conv1=L.Convolution2D(3,  96, 11)#, stride=)
 w = math.sqrt(2)  # MSRA scaling
 conv1=L.MLPConvolution2D(3, (96, 96, 96), 11, stride=4, wscale=w)
 
-cv1 = conv1(image)
-print(cv1.shape)
+mlpconv1 = conv1(image)
+print("conv result1:",mlpconv1.shape)
 
 #rev_image = np.transpose(cv1.data,(1,2,3,0))
 
 #plt.imshow(rev_image[0])
-type(cv1)
+type(mlpconv1)
 
-plt.imshow(cv1.data[0][0])
-cv1.data[0].shape
+print(mlpconv1.data[0].shape)
+print("mlpconv1 is")
+plt.imshow(mlpconv1.data[0][0])
 
 #n1, n2, h, w = model.conv1.W.shape
 fig = plt.figure()
@@ -55,6 +56,18 @@ fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
 for i in range(96):
     ax = fig.add_subplot(10, 10, i + 1, xticks=[], yticks=[])
 #    ax.imshow(model.conv1.W[i, 0], cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.imshow(cv1.data[0][i])#, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.imshow(mlpconv1.data[0][i])#, cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
+
+relu1 = F.relu(mlpconv1)
+print("relu result1:",relu1.data.shape)
+
+mp1 = F.max_pooling_2d(relu1, 3, stride=2)
+print("max_pooling result1:",mp1.data.shape)
+
+dr1 = F.dropout(mp1)
+print("dropout result1:",dr1.data.shape)
+
+
+#self.mlpconv4(h, train=self.train))
 
