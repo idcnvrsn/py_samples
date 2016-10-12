c932601df3a24dfba62332862130b677e87a2071
You <you@example.com>
2016-10-12 10:09:58 +0900

    決定境界を引いて確認するスクリプト追記

--- ojt/deeplearning1.r
+++ ojt/deeplearning1.r
@@ -104,3 +104,69 @@ table(dw$Result, predict(dm.svm.l, newdata=dw[,-1]))
    
 sum(diag(table(dw$Result, predict(dm.svm.l, newdata=dw[,-1]))))/nrow(dw)
 
+# データをインポートして、MXnetに読ませるための下処理と、プロット周りをグリッド含めて準備しておく
+dbi <- read.csv('https://github.com/ozt-ca/tjo.hatenablog.samples/raw/master/r_samples/public_lib/jp/dbi_large.txt', header=T, sep='\t')
+dbi$label <- dbi$label-1
+px <- seq(-4,4,0.03)
+py <- seq(-4,4,0.03)
+pgrid <- expand.grid(px,py)
+names(pgrid) <- names(dbi)[-3]
+dbi_train <- data.matrix(dbi)
+dbi_train.x <- dbi_train[,-3]
+dbi_train.y <- dbi_train[,3]
+dbi_test <- data.matrix(pgrid)
+label_grid <- rep(0, nrow(pgrid))
+for (i in 1:nrow(pgrid)){
+     if (pgrid[i,1]+pgrid[i,2]<0){label_grid[i] <- 1}
+}
+
+# MXnetで4-3-3中間層2層のDNNを回す
+data <- mx.symbol.Variable("data")
+fc1 <- mx.symbol.FullyConnected(data, name="fc1", num_hidden=4)
+act1 <- mx.symbol.Activation(fc1, name="tanh1", act_type="tanh")
+fc2 <- mx.symbol.FullyConnected(act1, name="fc2", num_hidden=3)
+act2 <- mx.symbol.Activation(fc2, name="tanh2", act_type="tanh")
+fc3 <- mx.symbol.FullyConnected(act2, name="fc3", num_hidden=3)
+act3 <- mx.symbol.Activation(fc3, name="tanh3", act_type="tanh")
+fc4 <- mx.symbol.FullyConnected(act3, name="fc4", num_hidden=2)
+softmax <- mx.symbol.SoftmaxOutput(fc4, name="softmax")
+devices <- mx.cpu()
+mx.set.seed(71)
+model <- mx.model.FeedForward.create(softmax, X=dbi_train.x, y=dbi_train.y, ctx=devices, num.round=20, array.batch.size=100, learning.rate=0.03, momentum=0.99,  eval.metric=mx.metric.accuracy, initializer=mx.init.uniform(0.5), array.layout = "rowmajor", epoch.end.callback=mx.callback.log.train.metric(100))
+Start training with 1 devices
+
+preds <- predict(model, dbi_test, array.layout = "rowmajor")
+pred.label <- max.col(t(preds)) - 1
+table(label_grid, pred.label)
+sum(diag(table(label_grid, pred.label)))/nrow(pgrid)
+
+# 描画する
+plot(c(), type='n', xlim=c(-4,4), ylim=c(-4,4), xlab='', ylab='')
+par(new=T)
+polygon(c(-4,4,4),c(4,-4,4),col='#dddddd')
+par(new=T)
+polygon(c(-4,-4,4),c(4,-4,-4),col='#ffdddd')
+par(new=T)
+plot(dbi[,-3], pch=19, cex=0.5, col=dbi$label+1, xlim=c(-4,4), ylim=c(-4,4), xlab='', ylab='')
+par(new=T)
+
+contour(px, py, array(pred.label, dim=c(length(px),length(py))), col='purple', lwd=5, levels =0.5, drawlabels=F, xlim=c(-4,4), ylim=c(-4,4))
+
+data <- mx.symbol.Variable("data")
+fc1 <- mx.symbol.FullyConnected(data, name="fc1", num_hidden=200)
+act1 <- mx.symbol.Activation(fc1, name="tanh1", act_type="tanh")
+fc2 <- mx.symbol.FullyConnected(act1, name="fc2", num_hidden=200)
+act2 <- mx.symbol.Activation(fc2, name="tanh2", act_type="tanh")
+fc3 <- mx.symbol.FullyConnected(act2, name="fc3", num_hidden=100)
+act3 <- mx.symbol.Activation(fc3, name="tanh3", act_type="tanh")
+fc4 <- mx.symbol.FullyConnected(act3, name="fc4", num_hidden=2)
+softmax <- mx.symbol.SoftmaxOutput(fc4, name="softmax")
+devices <- mx.cpu()
+mx.set.seed(71)
+model <- mx.model.FeedForward.create(softmax, X=dbi_train.x, y=dbi_train.y, ctx=devices, num.round=20, array.batch.size=100, learning.rate=0.03, momentum=0.99,  eval.metric=mx.metric.accuracy, initializer=mx.init.uniform(0.5), array.layout = "rowmajor", epoch.end.callback=mx.callback.log.train.metric(100))
+
+preds <- predict(model, dbi_test, array.layout = "rowmajor")
+pred.label <- max.col(t(preds)) - 1
+table(label_grid, pred.label)
+
+sum(diag(table(label_grid, pred.label)))/nrow(pgrid)
