8ac1e9d9f868493d1f47a3e9e401c499c22bd021
You <you@example.com>
2016-10-14 11:29:09 +0900

    詳細な検定を実施するスクリプト

--- learning_test/my_ttest.r
+++ learning_test/my_ttest.r
@@ -1,3 +1,13 @@
 x<-read.table("stat01.txt",header=T,sep=",")
 summary(x)
 t.test(x$A,x$C,var.equal=T)
+
+#³‹K«‚ÌŒŸ’èiKolmogorov-SmirnoviƒRƒƒ‚ƒSƒƒtEƒXƒ~ƒmƒtjŒŸ’èj
+ks.test(x$A,"pnorm",mean=mean(x$A),sd=sd(x$A))
+ks.test(x$C,"pnorm",mean=mean(x$C),sd=sd(x$C))
+
+#“™•ªŽU«‚ÌŒŸ’èiFŒŸ’èj
+var.test(x$A,x$C)
+
+t.test(x$A,x$C,var.equal=T)   # “™•ªŽU
+t.test(x$A,x$C,var.equal=F)   # •s“™•ªŽU
