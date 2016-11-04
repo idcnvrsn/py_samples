5f0c7f3c0e9c79c6faa029570b7b38e0d67d2a1c
You <you@example.com>
2016-11-04 16:54:08 +0900

    コメント追加

--- learning_test/anova.r
+++ learning_test/anova.r
@@ -3,6 +3,7 @@ x<-read.table("anova1way.txt",header=T,sep=",")
 F<-x$group     # F: 調理に使った脂肪質の種類
 y<-x$obs       # y: 測定値
 
+#P値が有意水準を下回っていることから，4水準の平均値は有意差が認められる。 
 s <- aov(y ~ F)
 anova(s)
 summary(aov(y ~ F))
